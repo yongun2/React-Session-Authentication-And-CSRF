19486d60628ac2486920dcd8daf1adb49a843fbd
yongun2 <djkye0001@naver.com>
2023-06-19 02:28:30 +0900

    [FEAT] Nav component 추가

--- src/pages/auth/LoginPage.jsx
+++ src/pages/auth/LoginPage.jsx
@@ -1,12 +1,14 @@
 import "../../styles/auth/LoginForm.css";
 import AuthTemplate from "../../components/templates/AuthTemplate.jsx";
 import LoginForm from "../../components/auth/LoginForm.jsx";
+import AuthNav from "../../components/auth/AuthNav.jsx";
 
 function LoginPage() {
     return (
         <AuthTemplate>
             <h1>로그인</h1>
             <LoginForm/>
+            <AuthNav/>
         </AuthTemplate>
 
     );
